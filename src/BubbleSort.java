e51738dc073d30a9e42fe094d6f0f4dcbf98dbc9
Do Duc Kiet <dokiet2k6@gmail.com>
2025-10-03 22:02:57 +0700

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,34 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] a) {
+        int n = a.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] a = {64, 34, 25, 12, 22, 11, 90};
+
+        bubbleSort(a);
+
+        printArray(a);
+    }
+}
